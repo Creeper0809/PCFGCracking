b769e423488ed3025e0f0d7806633a3bf3d84b92
creeper0809 <creeper0809@hotmail.com>
2025-06-19 12:45:56 +0900

    asd

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+def load_requirements(filename):
+    with open(filename, encoding="utf-8", errors="ignore") as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith("#") and not line.startswith("-")
+        ]
+
+setup(
+    name="pcfg_lib",
+    version="0.1",
+    packages=["lib"],
+    package_dir={"": "."},
+    include_package_data=True,
+    install_requires=load_requirements("requirements.in"),
+)
